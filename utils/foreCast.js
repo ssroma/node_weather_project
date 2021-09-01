812ecc84432e25a56d93ca5c308813b3c674fbb8
Sergio Roma <roma.sergio@gmail.com>
2021-09-01 18:53:01 +0100

    Comments removed, little clean up

--- utils/foreCast.js
+++ utils/foreCast.js
@@ -1,17 +1,4 @@
 const request = require('request');
-/*
-/ WeatherStack API
-/ http://api.weatherstack.com/current
-/ 4547ba7eecec951f6e026f6c4c9f3d79
-/ access_key
-/ http://api.weatherstack.com/current?access_key=4547ba7eecec951f6e026f6c4c9f3d79&query="Itambe"
-*/
-
-
-// Weather Stack API
-const WEATHER_STACK_ACCESS_KEY = `4547ba7eecec951f6e026f6c4c9f3d79`;
-const location = 'Itambe';
-//const weatherStackUrl = `http://api.weatherstack.com/current?access_key=${WEATHER_STACK_ACCESS_KEY}&query=${location}`;
 
 const foreCast = (location, callBack) => {
     const url = `http://api.weatherstack.com/current?access_key=4547ba7eecec951f6e026f6c4c9f3d79&query=${location}`;
--- utils/geoLocation.js
+++ utils/geoLocation.js
@@ -1,16 +1,4 @@
 const request = require('request');
-/*
-/ MapBox 
-/ http://api.weatherstack.com/current
-/ acess_key = pk.eyJ1Ijoic2VyZ2lvcm9tYSIsImEiOiJja3NzMGxxbzAwcnFpMndvZGczYnVhZmJlIn0.hcqXX78YDm8HD6jUaAr79g
-/ https://api.mapbox.com/geocoding/v5/mapbox.places/vitoria%20da%20consquista.json?access_token=pk.eyJ1Ijoic2VyZ2lvcm9tYSIsImEiOiJja3NzMGxxbzAwcnFpMndvZGczYnVhZmJlIn0.hcqXX78YDm8HD6jUaAr79g&limit=1
-*/
-
-// Geo Location MapBox API 
-//const GEO_ACCESS_KEY = "pk.eyJ1Ijoic2VyZ2lvcm9tYSIsImEiOiJja3NzMGxxbzAwcnFpMndvZGczYnVhZmJlIn0.hcqXX78YDm8HD6jUaAr79g";
-//const queryResultLimit = 1;
-//const geoLocation = "Itambe BA";
-//const geoUrl = `https://api.mapbox.com/geocoding/v5/mapbox.places/${geoLocation}.json?access_token=${GEO_ACCESS_KEY}&limit=${queryResultLimit}`;
 
 const geoLocationCode = (location, params, callBack) => {
     const url = `https://api.mapbox.com/geocoding/v5/mapbox.places/${location}.json?access_token=pk.eyJ1Ijoic2VyZ2lvcm9tYSIsImEiOiJja3NzMGxxbzAwcnFpMndvZGczYnVhZmJlIn0.hcqXX78YDm8HD6jUaAr79g&limit=${params}`;
